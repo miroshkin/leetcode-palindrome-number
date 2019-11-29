5d876f8bf459e235a06e3a6e3d006429c5239309
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-11-29 16:13:36 +0300

    Init project

---
+++ Leetcode_PalindromeNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Leetcode_PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int a = 121;
+            if (solution.IsPalindrome(a))
+            {
+                Console.WriteLine($"Int {a} is palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"Int {a} is not a palindrome");
+            }
+        }
+    }
+
+    public class Solution {
+        public bool IsPalindrome(int x) {
+            string s1 = x.ToString();
+            char[] ca = s1.ToCharArray();
+
+            Array.Reverse(ca);
+            string s2 = ca.ToString();
+        
+            if (s1.CompareTo(s2) == 0)
+            {
+                return true;
+            }
+        
+            else 
+            {
+                return false;
+            }
+        }
+    }
+}
